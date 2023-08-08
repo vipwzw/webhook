270958bf1d2b7fce9c9697885b51bc583728f7ee
wang <wzw@disanbo.com>
2023-08-08 18:23:12 +0800

    add read table example

--- 1.js
+++ 1.js
@@ -22,19 +22,46 @@ function playAudio(file, times) {
       }
     });
   }
-async function waitUntilPageLoaded() {
-    await driver.get("https://www.12306.cn/index/");
-    playAudio("/Users/kingwang/Downloads/sample-6s.mp3", 10)
-    await delay(300)
 
-    await driver.findElement(By.linkText("密码登录")).click()
-    await delay(1)
-    await driver.findElement(By.id("agree")).click()
-    await delay(1)
-    await driver.findElement(By.css(".Button_button_3onsJ")).click()
-    await delay(5)
-    // 在这里执行您的其他操作
-    await driver.quit()
+// 找到所有行
+async function getTableData(table) {
+    // 获取所有行
+    const rows = await table.findElements(By.css('tr'));
+
+    // 创建一个空数组来存储表格内容
+    const tableData = [];
+
+    // 遍历行和单元格，并将内容添加到数组
+    for (const row of rows) {
+      const cells = await row.findElements(By.css('td'));
+      const rowData = await Promise.all(cells.map(cell => cell.getText()));
+      tableData.push(rowData);
+    }
+    return tableData
 }
 
+async function waitUntilPageLoaded() {
+    await driver.get("https://www.12306.cn/index/");
+    await driver.wait(until.urlContains("https://kyfw.12306.cn/otn/leftTicket/init?linktypeid=dc"),  600000);
+    await delay(30)
+
+    while(true) {
+      try {
+        await driver.findElement(By.linkText("查询")).click()
+        await delay(2)
+        // Find the parent <div> element using CSS selector
+        const parentDiv = await driver.findElement(By.css('#t-list'));
+        // Find the <table> element within the parent <div> using CSS selector
+        const table = await parentDiv.findElement(By.css('table'));
+        var data = await getTableData(table)
+        if hasTicket(data) {
+           playAudio("./sample-6s.mp3", 10)
+        }
+        console.log(data)
+      } catch (e) {
+         console.log(e)
+      }
+    }
+    await driver.quit()
+}
 waitUntilPageLoaded();
